916c5284f58cc411f147a4a8a5d1a8a6d2f6c7e7
musayann <musayann@octangrp.com>
2021-10-12 16:49:50 +0200

    chore: add table layout

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -42,7 +42,7 @@ function Table({ columns, data }: { columns: any[]; data: any[] }) {
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
             <table
-              className="min-w-full divide-y divide-gray-200"
+              className="w-full divide-y divide-gray-200 table-auto"
               {...getTableProps()}
             >
               <Header headerGroups={headerGroups} />
