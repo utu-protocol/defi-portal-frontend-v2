6b9317b7c0c3f4b4ce2c190ad5ea386b7f8b4b9b
gabrielaacha <gabriela.gabriela.acha.acha@gmail.com>
2022-01-07 18:03:16 +0100

    styling for the defi portal page

--- src/components/trustEngine.tsx
+++ src/components/trustEngine.tsx
@@ -31,18 +31,17 @@ export default function TrustEngine({
   const itemsToShow = (summaryImages || []).slice(0, countToShow)
   const itemsToHide = (summaryImages || []).length - itemsToShow.length
   return <div className=''>
-    <div className='text-sm leading-5 text-gray-700'>
-      {summaryText}
-      {summaryText == null ? '' : <x-utu-root source-uuid={address} target-type='product'>
-        <x-utu-feedback-details-popup target-uuid={protocolAddress}>
+    <div className='flex justify-around text-sm leading-5'>
+      <div className="mx-8" >{summaryText}</div>
+      <div className="flex flex-col w-80">
+      {summaryText == null ? '' : <div><x-utu-root source-uuid={address} target-type='product'>
+      <x-utu-feedback-details-popup target-uuid={protocolAddress}>
         </x-utu-feedback-details-popup>
         {summaryText.includes('You used') ?
-          <x-utu-feedback-form-popup source-uuid={address} target-uuid={protocolAddress} transaction-id={address}>
+         <x-utu-feedback-form-popup source-uuid={address} target-uuid={protocolAddress} transaction-id={address}>
           </x-utu-feedback-form-popup> : ''}
-      </x-utu-root>}
-    </div>
-
-    {itemsToShow.length > 0 ? (<div className='flex items-center space-x-2 mt-2'>
+      </x-utu-root></div>}
+      {itemsToShow.length > 0 ? (<div className='flex items-center space-x-2 mt-2'>
       <div className='flex flex-shrink-0 space-x-2'>
         {itemsToShow.map((item) => {
           const address = item.substring(item.length - ethereumAddressLength)
@@ -57,6 +56,24 @@ export default function TrustEngine({
       }
 
     </div>) : null}
+    </div>
+    </div>
+
+    {/* {itemsToShow.length > 0 ? (<div className='flex items-center space-x-2 mt-2'>
+      <div className='flex flex-shrink-0 space-x-2'>
+        {itemsToShow.map((item) => {
+          const address = item.substring(item.length - ethereumAddressLength)
+          // return <img key={index} className={`max-w-none h-6 w-6 rounded-full text-utu-100 shadow-solid`} src={item} alt="" />
+          return <span key={address} className='block'><UserAvatar size={32} address={address} /></span>
+        })}
+
+      </div>
+
+      {
+        itemsToHide > 0 && <span className='flex-shrink-0 text-xs leading-5 font-medium'>+{itemsToHide}</span>
+      }
+
+    </div>) : null} */}
 
 
   </div>
